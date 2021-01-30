509998b71ed1601a314c0c6c21b74a9acd02b262
Bob O'Connor <robert.t.oconnor@kodakalaris.com>
2021-01-30 13:29:47 -0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
+import { saveAs } from 'file-saver';
 import './App.css';
+const JSZip = require('jszip')
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <button onClick={bob}>Go zip something! </button>
       </header>
     </div>
-  );
-}
+ );
+ 
+  async function bob() { 
+    
+    const zip = new JSZip();
+    zip.file("test", "Hello");
+    var img= zip.folder("images");
+    img.file("logo.svg", logo, {base64: false});
+  
+    try{ 
+      await zip.generateAsync({type:"blob"}).then(content => {   saveAs.saveAs(content,"test.zip")});
+      console.log("OK.. zipped maybe.  ")} 
+    catch {
+     console.log("ERROR")
+    }
+    
+    }
 
+    
+}
 export default App;
+
+//.then(content => {   saveAs.saveAs(content,"test.zip")});
